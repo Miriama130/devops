a6c8bcbfb0c7d48db67461a87eabcbbc22858100
miriama130 <tlili.mariem.1@esprit.tn>
2025-02-24 15:29:48 +0000

    unit test

--- src/main/java/tn/esprit/spring/FoyerApplication.java
+++ src/main/java/tn/esprit/spring/FoyerApplication.java
@@ -1,4 +1,4 @@
-package tn.esprit.spring.FoyerApplication;
+package tn.esprit.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
