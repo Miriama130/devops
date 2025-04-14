4246184abc464f0bdcb99a183817031100888cd3
sfaradam <sfar.adam@esprit.tn>
2025-04-14 19:07:29 +0100

    jenkins fix

--- src/main/java/tn/esprit/spring/FoyerApplication.java
+++ src/main/java/tn/esprit/spring/FoyerApplication.java
@@ -3,9 +3,10 @@ package tn.esprit.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 @SpringBootApplication
+
 @EnableScheduling
+
 public class FoyerApplication {
 
     public static void main(String[] args) {
