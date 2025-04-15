0212291495209ed5339fba69d3c5a4594c4fbea4
sfaradam <sfar.adam@esprit.tn>
2025-04-15 15:21:04 +0100

    jenkins fix

---
+++ src/test/java/tn/esprit/spring/RestControllers/FoyerRestControllerTest.java
@@ -0,0 +1,39 @@
+package tn.esprit.spring.RestControllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class FoyerRestControllerTest {
+
+    @Test
+    void addOrUpdate() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void affecterFoyerAUniversite() {
+    }
+
+    @Test
+    void desaffecterFoyerAUniversite() {
+    }
+
+    @Test
+    void ajouterFoyerEtAffecterAUniversite() {
+    }
+}
