cea860b1a3d238b1e8fa32d3943b777228a1c6eb
Mahmoud-Belghaieb <mahmoud.belghaieb@gmail.com>
2025-04-18 03:55:11 +0100

    back to test uni with Junit Mok,Jenkins file not updated

--- src/test/java/tn/esprit/spring/BlocServiceTest.java
+++ src/test/java/tn/esprit/spring/BlocServiceTest.java
@@ -1,11 +1,15 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.spring.DAO.Entities.Bloc;
 import tn.esprit.spring.DAO.Entities.Chambre;
 import tn.esprit.spring.DAO.Entities.Foyer;
@@ -22,6 +26,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
 class BlocServiceTest {
 
     @Mock
